1adb4b783640c79a0b5ae99d0da3227be3123369
J. Alejandro Olivares Padilla <86990559+AlexOlivaresP@users.noreply.github.com>
2021-12-10 18:53:41 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,19 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conexion = new PDO("sqlsrv:server = tcp:escom.database.windows.net,1433; Database = escom", "escom", "Empanada123?");
-    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+use FFI\Exception;
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "escom", "pwd" => "Empanada123?", "Database" => "escom", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:escom.database.windows.net,1433";
-$conexion = sqlsrv_connect($serverName, $connectionInfo);
+$servername = "escom.database.windows.net";
+$username = "escom";
+$password = "Empanada123?";
+$database = "escom";
+$port= "1433";
+
+// Create connection
+$conexion = new mysqli($servername, $username, $password, $database, $port);
+
+// Check connection
+if ($conexion->connect_error) {
+    die("Connection failed: " . $conexion->connect_error);
+} else {
+    /*echo "Connected successfully";*/
+}
 ?>
